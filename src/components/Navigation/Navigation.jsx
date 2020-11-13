5f5d430367856bd658719fe84bf65e0442256087
waveslin <35038769+waveslin@users.noreply.github.com>
2020-11-13 05:12:20 -0500

    add Navigation test

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,21 +10,21 @@ const Navigation = ()=> {
     // className={`nav-logo${toggle ? '':' menu-logo'}`}
     return (
         <>
-            <nav>
-                <div className='nav-logo'>
+            <nav data-test='Navigation-nav'>
+                <div className='nav-logo' data-test='logo-img'>
                     <Link to='/' onClick={resetToggle}>
                         <img src="rented-logo.png" alt=""/>
                     </Link>
                 </div>
 
-               <button className={`nav-toggle toggle-icon sm-menu${toggle? '': ' hide'}`} id="toggle_icon" data-menu="menu" onClick={setToggle}>
+               <button className={`nav-toggle toggle-icon sm-menu${toggle? '': ' hide'}`} id="toggle_icon" data-menu="menu" onClick={setToggle} data-test='Navigation-menu-toggle'>
                     <i className="fas fa-bars"></i>
                </button>
 
-                <button className={`menu-exit toggle-icon${toggle ? ' hide' : ' show'}`} onClick={setToggle}>
+                <button className={`menu-exit toggle-icon${toggle ? ' hide' : ' show'}`} onClick={setToggle} data-test='Navigation-menu-close'>
                     <i className="fas fa-times-circle"></i>
                 </button>
-                <ul className={`nav-list${toggle ? ' hide' : ' show'}`} id="menu">
+                <ul className={`nav-list${toggle ? ' hide' : ' show'}`} id="menu" data-test='Navigation-link-list'>
                     <li><Link to='/' onClick={resetToggle}>Home</Link></li>
                     <li><Link to='/contact' onClick={resetToggle}>Contact</Link></li>
                     <li><Link to='/signup' onClick={resetToggle}>Sign up</Link></li>
---
+++ src/components/Navigation/Navigation.test.js
@@ -0,0 +1,39 @@
+import Navigation from './Navigation.jsx';
+import Adaptor from 'enzyme-adapter-react-16';
+import {shallow, configure} from 'enzyme';
+import {findByDataTest} from '../../util/testHelper';
+
+configure({adapter: new Adaptor()});
+
+const setup = (props={}) => {
+    return shallow(<Navigation {...props} />);
+}
+
+describe('Navigation Test', ()=>{
+    let component;
+    beforeEach(()=>{
+        component = setup();
+    });
+
+    it('Navigation used nav as container', ()=>{
+        let sample = findByDataTest(component, 'Navigation-nav');
+        expect(sample.length).toBe(1);
+    });
+
+
+    it('Navigation has the link list ', ()=>{
+        let sample = findByDataTest(component, 'Navigation-link-list');
+        expect(sample.length).toBe(1);
+    });
+
+    it('Navigation has the small screen menu toggle button', ()=>{
+        let sample = findByDataTest(component, 'Navigation-menu-toggle');
+        expect(sample.length).toBe(1);
+    });
+
+    it('Navigation has the small screen menu close button', ()=>{
+        let sample = findByDataTest(component, 'Navigation-menu-close');
+        expect(sample.length).toBe(1);
+    });
+
+});
